8ec1cf997e4d9909ed3db11aaba50c7ccba31110
Michelle G <gerbermichelle23@gmail.com>
2019-04-23 19:50:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 function dwarfRollCall(dwarves) {
+  var dwarfnames = [];
+  dwarves.forEach(function(names, i) {
+    dwarfnames += ([i+1]+'. '+ names + " ");
+  });
+  return dwarfnames;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var veggies = [];
+   for (var i = 0; i < planeteerCalls.length; i++){
+ veggies.push(planeteerCalls[i].toUpperCase() + '!')
 }
-
+  return veggies;
+}
 function longPlaneteerCalls(words) {
+  if (words.length < 4) {
+    return false
+  } else {
+    return true
+  }
 }
 
 function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; i++) {
+      if (foods[i] === 'cheddar') {
+      return foods[i]
+    }
+  }
+  return 'no cheese!'
 }
