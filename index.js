804a2fc2de3c8153fa1bb1bbf4a71299681e0373
Michelle G <gerbermichelle23@gmail.com>
2019-07-17 18:56:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // f  declaration + f name and params in() defines 
 function dwarfRollCall(dwarves) {
   var dwarfnames = [];
-  //f expression IIFE
+  //f expression IIFE  
   dwarves.forEach(function(names, i) {
     dwarfnames += ([i+1]+'. '+ names + " ");
   });
